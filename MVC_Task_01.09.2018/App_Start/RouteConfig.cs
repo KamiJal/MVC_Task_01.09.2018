44588e6b31e24e0681690860042d6b705dd0b37c
Kamil <ushurbakiev.k@gmail.com>
2018-09-22 22:08:20 +0600

    AddedRoutesByYearByMonthByDay

--- MVC_Task_01.09.2018/App_Start/RouteConfig.cs
+++ MVC_Task_01.09.2018/App_Start/RouteConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Routing.Constraints;
 using System.Web.Routing;
 
 namespace MVC_Task_01._09._2018
@@ -13,7 +14,50 @@ namespace MVC_Task_01._09._2018
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute(
+                name: "GetStopsByDay",
+                url: "Stops/{year}/{month}/{day}",
+                defaults: new { controller = "Stops", action = "GetStopsByDay" },
+                constraints: new
+                {
+                    year = @"\d{4}",
+                    month = new CompoundRouteConstraint(
+                        new List<IRouteConstraint>
+                        {
+                            new LengthRouteConstraint(2),
+                            new RangeRouteConstraint(1, 12)
+                        }),
+                    day = new CompoundRouteConstraint(
+                        new List<IRouteConstraint>
+                        {
+                            new LengthRouteConstraint(2),
+                            new RangeRouteConstraint(1, 31)
+                        })
+                }
+            );
 
+            routes.MapRoute(
+                name: "GetStopsByMonth",
+                url: "Stops/{year}/{month}",
+                defaults: new { controller = "Stops", action = "GetStopsByMonth" },
+                constraints: new
+                {
+                    year = @"\d{4}",
+                    month = new CompoundRouteConstraint(
+                        new List<IRouteConstraint>
+                        {
+                            new LengthRouteConstraint(2),
+                            new RangeRouteConstraint(1, 12)
+                        })
+                }
+            );
+
+            routes.MapRoute(
+                name: "GetStopsByYear",
+                url: "Stops/{year}",
+                defaults: new { controller = "Stops", action = "GetStopsByYear" },
+                constraints: new { year = @"\d{4}" }
+            );
 
             routes.MapRoute(
                 name: "Default",
--- MVC_Task_01.09.2018/Controllers/StopsController.cs
+++ MVC_Task_01.09.2018/Controllers/StopsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,6 +28,37 @@ namespace MVC_Task_01._09._2018.Controllers
             var stops = _context.TrackServiceHistory.Take(100).ToList();
 
             return View(stops);
+        }
+
+        // GET: Stops/{year}
+        public ActionResult GetStopsByYear(int year)
+        {
+            var stops = _context.TrackServiceHistory.Where(stop => stop.dRepairDate.Year == year).Take(100).ToList();
+
+            return View("Index", stops);
+        }
+
+        // GET: Stops/{year}/{month}
+        public ActionResult GetStopsByMonth(int year, int month)
+        {
+            var stops = _context.TrackServiceHistory.Where(stop => stop.dRepairDate.Year == year && stop.dRepairDate.Month == month).Take(100).ToList();
+
+            return View("Index", stops);
+        }
+
+        // GET: Stops/{year}/{month}/{day}
+        public ActionResult GetStopsByDay(int year, int month, int day)
+        {
+            IEnumerable<TrackServiceHistory> stops;
+
+            if (DateTime.TryParse($"{month}/{day}/{year}", CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+                stops = _context.TrackServiceHistory.ToList()
+                    .Where(stop => stop.dRepairDate.Date == date.Date)
+                    .Take(100);
+            else
+                return HttpNotFound("DATE CANNOT BE PARSED!");
+
+            return View("Index", stops);
         }
     }
 }
